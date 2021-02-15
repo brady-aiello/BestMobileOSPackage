67a8efedbea9e4fd126c181f2626d95a830b79dd
Brady Aiello <brady@touchlab.co>
2021-02-15 12:20:15 -0800

    First code commit.

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "BestMobileOS",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BestMobileOS",
+            targets: ["BestMobileOS"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BestMobileOS",
+            path: "./BestMobileOS.xcframework"
+        ),
+    ]
+)
